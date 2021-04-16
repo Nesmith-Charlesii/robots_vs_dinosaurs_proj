95159eac4331e82017ca0d2aea54f90b110ec902
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-16 12:28:51 -0400

    created name, health, power_level, weapon_type, and attack_power as properties of the Robot class in the robot_class.py file

---
+++ robot_class.py
@@ -0,0 +1,9 @@
+class Robot:
+    def __init__(self):
+        self.name = " "
+        self.health = " "
+        self.power_level = " "
+        self.weapon_type = " "
+        self.attack_power = " "
+
+    
