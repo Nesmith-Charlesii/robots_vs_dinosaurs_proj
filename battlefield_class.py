56b49d90e6c2728c538b0bfda142f1e33e33155c
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-19 00:36:24 -0400

    created a battlefield list in the set_battlefield method of the battlefield class. Randomly selects a location from a list, or the user can input their own.

--- battlefield_class.py
+++ battlefield_class.py
@@ -1,3 +1,13 @@
+import random
+
 class Battlefield:
     def __init__(self):
-        self.battlefield_location = '2098 jurassic park'
+        self.battlefield_location =
+        self.fleets = []
+        self.herds = []
+
+    def set_battlefield(self):
+        battlefield_list = ['2098 jurassic park', 'prehistoric terrain', 'modern day new york', 'onsite devCodeCamp']
+        rand = random.randrange(0, 3)
+        self.battlefield_location = battlefield_list[rand]
+        return self.battlefield_location
